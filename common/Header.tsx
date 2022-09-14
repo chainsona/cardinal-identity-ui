6abbf1c96210dc4bf97b468f552dc793f4367610
Avinash Jain <avinashkalpitjain@gmail.com>
2022-09-14 01:11:11 -0700

    Updated header to match rentals

--- common/Header.tsx
+++ common/Header.tsx
@@ -1,49 +1,79 @@
-import styled from '@emotion/styled'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { LogoTitled } from 'lib/src/common/LogoTitled'
+import { AccountConnect } from '@cardinal/namespaces-components'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { useWalletModal } from '@solana/wallet-adapter-react-ui'
+import { GlyphWallet } from 'assets/GlyphWallet'
+import { useRouter } from 'next/router'
 import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
+import { useEffect, useState } from 'react'
 
-const WalletButton = styled(WalletMultiButton)`
-  color: rgba(255, 255, 255, 0.8);
-  font-size: 14px;
-  z-index: 10;
-  height: 38px;
-  border: none;
-  background: none;
-  background-color: none;
-  &:hover {
-    background: none !important;
-    background-color: none !important;
-  }
-`
+import { ButtonSmall } from './ButtonSmall'
+import { LogoTitled } from './LogoTitled'
+import { asWallet } from './Wallets'
 
-export const Header = () => {
-  const { environment } = useEnvironmentCtx()
+type Props = {
+  tabs?: {
+    disabled?: boolean
+    name: string
+    anchor: string
+    tooltip?: string
+  }[]
+  hideDashboard?: boolean
+}
+
+
+
+
+export const Header: React.FC<Props> = ({ tabs, hideDashboard }: Props) => {
+  const router = useRouter()
+  const wallet = useWallet()
+  const walletModal = useWalletModal()
+  const { connection, environment } = useEnvironmentCtx()
+
+  const [tab, setTab] = useState<string>('browse')
+
+  useEffect(() => {
+    const anchor = router.asPath.split('#')[1]
+    if (anchor !== tab) setTab(anchor || 'browse')
+  }, [router.asPath, tab])  
+
   return (
-    <div className="flex flex-row justify-between p-10">
-      <div className="flex flex-row">
-        <div className="my-auto flex flex-col">
-          <LogoTitled className="inline-block h-6" />
-        </div>
-        <div className="absolute top-[60px] left-[200px] text-[10px] italic text-white">
-          <span
-            className="mr-2 rounded-md px-[7px] py-1"
-            style={{ background: '#FFFFFF30' }}
+    <div className="w-full px-4 py-4">
+      <div className="flex min-h-[72px] flex-wrap items-center justify-center gap-4 rounded-xl bg-white bg-opacity-5 py-4 px-8 md:justify-between">
+        <div className="flex items-center gap-5">
+          <div
+            className="flex cursor-pointer items-center transition-opacity hover:opacity-60"
+            onClick={() => {             
+              router.push(`/${location.search}`)
+            }}
           >
-            identity
-          </span>
+              <LogoTitled className="inline-block h-6" />
+          </div>
           {environment.label !== 'mainnet-beta' && (
-            <span
-              className="rounded-md px-[7px] py-1"
-              style={{ background: '#FFFFFF30', transform: 'translateY(20%)' }}
-            >
-              {environment.label}
-            </span>
+            <div className="text-primary">{environment.label}</div>
           )}
         </div>
-      </div>
-      <div className="flex flex-row">
-        <WalletButton />
+        
+        <div className="flex-5 flex items-center justify-end gap-6">          
+          {wallet.connected && wallet.publicKey ? (
+            <AccountConnect
+              dark={true}
+              connection={connection}
+              environment={environment.label}
+              handleDisconnect={() => wallet.disconnect()}
+              wallet={asWallet(wallet)}
+            />
+          ) : (
+            <ButtonSmall
+              className="text-xs"
+              onClick={() => walletModal.setVisible(true)}
+            >
+              <>
+                <GlyphWallet />
+                <div className="text-white">Connect wallet</div>
+              </>
+            </ButtonSmall>
+          )}
+        </div>
       </div>
     </div>
   )
